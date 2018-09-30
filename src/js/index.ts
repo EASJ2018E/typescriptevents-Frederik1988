e9a644efd0eec76cf2310f6023247c45b502d92a
Frederik Skærbæk Jensen <fred15b4@edu.easj.dk>
2018-09-30 23:21:14 +0200

    Jeg skal bruge hjælp til at lave en function til hidden. Jeg har prøvet mange ting af, men det er kun lykkedes mig at gemme hele listen (ikke enkelte elementer

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,20 @@
-//Her skal du skrive din typescriptkode
+document.getElementById("addbtn").addEventListener("click",addGrocery)
+
+
+function addGrocery(){
+    let count = 1
+    let inputElement = document.getElementById("addText")
+    let inputText = (<HTMLInputElement>inputElement).value
+
+    let newLi = document.createElement("li")
+    let newText = document.createTextNode(inputText.toString())
+    newLi.setAttribute("class", "usundt")
+    newLi.appendChild(newText)
+    document.getElementById("listeUsund").appendChild(newLi)
+    
+    for (let i = 0; i< document.getElementById("listeUsund").childElementCount; i++){
+        document.getElementById("listeUsund").children[i].id = "vare" + count
+        count++
+    }
+    
+}
